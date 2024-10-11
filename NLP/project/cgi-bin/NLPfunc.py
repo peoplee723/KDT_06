2ee67a5148384fae2e9bbdce67028973a41dac78
peoplee723 <aukii21@naver.com>
2024-10-11 13:33:59 +0900

    project

---
+++ NLP/project/cgi-bin/NLPfunc.py
@@ -0,0 +1,100 @@
+#####
+from collections import Counter
+import torch.nn as nn
+from typing import Literal
+import torch.nn.functional as F
+
+def vectorize(vocab, DF, tokenizer):
+    '''
+    단어사전을 통해 문장을 수치화하는 함수
+
+    '''
+    vector_list=[]
+  
+    for t in DF:
+        token_lists=tokenizer.morphs(t)
+        vector_token=[vocab[token] if token in vocab else vocab['<UNK>'] for token in token_lists]
+        vector_list.append(vector_token)
+
+    return vector_list
+
+class textCLF(nn.Module):
+    def __init__(self, n_vocab, hidden_dim, embedding_dim, n_layers,
+                 model_type: Literal['lstm', 'rnn'], dropout=0.5, bidirectional=True):
+        super().__init__()
+
+        # 임베딩 층
+        # num-> 단어사전의 크기
+        self.embedding= nn.Embedding(
+            num_embeddings=n_vocab,
+            embedding_dim=embedding_dim,
+            padding_idx=0
+        )
+        if model_type =='rnn':
+            self.model=nn.RNN(
+                input_size=embedding_dim,
+                hidden_size=hidden_dim,
+                num_layers=n_layers,
+                bidirectional=bidirectional,
+                dropout=dropout,
+                batch_first=True
+            )
+        elif model_type =='lstm':
+            self.model= nn.LSTM(
+                input_size=embedding_dim,
+                hidden_size=hidden_dim,
+                num_layers=n_layers,
+                bidirectional=bidirectional,
+                dropout=dropout,
+                batch_first=True
+            )
+        # 양방향 여부
+        if bidirectional:
+            self.classifier= nn.Linear(hidden_dim*2, 3)
+        else:
+            self.classifier= nn.Linear(hidden_dim, 3)
+        self.dropout=nn.Dropout(dropout)
+
+    #포워딩
+    def forward(self, input):
+        embeddings= self.embedding(input)
+        output,_= self.model(embeddings)
+        last_output=output[:,-1,:]
+        last_output=self.dropout(last_output)
+        logits=self.classifier(last_output)
+        return logits
+    
+
+def vectorize2(vocab, DF, tokenizer):
+    '''
+    단어사전을 통해 문장을 수치화하는 함수
+
+    '''
+    vector_list=[]
+  
+    for t in DF:
+        token_lists=tokenizer.morphs(t)
+        vector_token=[vocab[token] if token in vocab else vocab['<UNK>'] for token in token_lists]
+        vector_list.extend(vector_token)
+
+    return vector_list
+
+
+def padding2(length, text):
+    pad_texts=[]
+    # for text in textList:
+    #         # 선택 길이> 문장길이 일때
+    if length>len(text):
+                                        # 남은 텍스트 길이만큼 0으로 채우기
+        text=text+[0]*(length-len(text))
+    else: # 선택 길이< 문장 길이 일때
+        text=text[:length]              # 선택 길이만큼 슬라이싱
+    pad_texts.append(text)
+    return pad_texts
+
+def predict_mcf2(model, data, result):
+    dataTS=data.reshape(1,-1) #torch.FloatTensor(data).reshape(1,-1)
+    pre_val=model(dataTS)
+    pre_val=F.softmax(pre_val, dim=1)
+    print(f'{result[0]}: {max(pre_val[0].detach()):.4f}')
+    return f'{result[0]}: {max(pre_val[0].detach()):.4f}'
--- crawling/Day03/naverapi_3.py
+++ crawling/Day03/naverapi_3.py
@@ -1,79 +1,19 @@
-import urllib.parse
+# 네이버 검색 API 예제 - 블로그 검색
+import os
+import sys
 import urllib.request
-import datetime
-import json
-import pandas as pd
-
-def get_request_url(url):
-    client_id= 'vAcuTP8HYLTwb34u12a0'
-    client_secret= 'tcRd1JyRAC'
-
-    req= urllib.request.Request(url)
-    req.add_header('X-Naver-Client-Id', client_id)
-    req.add_header('X-Naver-Client-Secret', client_secret)
-
-    try:
-        response= urllib.request.urlopen(req)
-        if response.getcode() ==200:
-            return response.read().decode('utf-8')
-    except Exception as e:
-        print(e)
-        print(f'Error for URL: {url}')
-
-def get_naver_search(node, search_text, start, display):
-    base= "https://openapi.naver.com/v1/search"
-    node= f'/{node}.json'
-    query_string= f'{urllib.parse.quote(search_text)}'
-
-    parameters= ('?query={}&start={}&display={}'.
-                 format(query_string, start, display))
-    url= base+ node+ parameters
-    response= get_request_url(url)
-
-    if response is None:
-        return None
-    else:
-        return json.loads(response)
-
-def get_post_data(post, json_result_list, cnt):
-    title= post['title']
-    description= post['description']
-    org_link= post['originallink']
-    link= post['link']
-    '''
-					strptime()
-					- %a:	abbreviated	weekday	name					
-					- %b"	abbreviated	month	name
-				'''
-    pdate= datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
-    pdate= pdate.strftime('%Y-%m-%d %H:%M:%S')
-
-    print(f'[{cnt}]', end=' ')
-    print(title, end=': ')
-    print(pdate, end=' ')
-    print(link)
-
-    json_result_list.append([cnt, pdate, title, description, org_link, link])
-
-def main():
-    node= 'news' #크롤링 대상
-    search_text= '인공지능'
-    cnt= 0
-    json_result_list= []
-
-    json_response= get_naver_search(node, search_text, 1, 100)
-    while (json_response is not None) and (json_response['display'] !=0):
-        for post in json_response['items']:
-            cnt+=1
-            get_post_data(post, json_result_list, cnt)
-
-        start= json_response['start']+json_response['display']
-        json_response= get_naver_search(node, search_text, start, 100)
-    print(f'전체 검색 수: {cnt}')
-    columns= ['count', 'date', 'title', 'description', 'org_link', 'link']
-    result_df= pd.DataFrame(json_result_list, columns=columns)
-    result_df.to_csv(f'{search_text}_naver_{node}.csv', index=False, encoding='utf-8')
-
-if __name__ == '__main__':
-    main()
-
+client_id = "vAcuTP8HYLTwb34u12a0"
+client_secret = "NwFeqYc5YY"
+encText = urllib.parse.quote("")
+url = "https://openapi.naver.com/v1/breakingnews/section/105/226/query=" + encText # JSON 결과
+# url = "https://openapi.naver.com/v1/search/news.xml?query=" + encText # XML 결과
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+response = urllib.request.urlopen(request)
+rescode = response.getcode()
+if(rescode==200):
+    response_body = response.read()
+    print(response_body.decode('utf-8'))
+else:
+    print("Error Code:" + rescode)
